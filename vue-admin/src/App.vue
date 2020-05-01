0dd8226d707a863650418095656d4239ebe90d88
akrito <vipchenpost@163.com>
2020-05-01 22:06:32 +0800

    init project and import elementUI

---
+++ vue-admin/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+    >
+      <el-menu-item index="1">
+        <router-link to="/">Home</router-link>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <router-link to="/about">About</router-link>
+      </el-menu-item>
+
+      <el-button type="primary">添加用户</el-button>
+    </el-menu>
+
+    <router-view />
+  </div>
+</template>
+
+<style>
+.el-row {
+  margin-bottom: 20px;
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+</style>
